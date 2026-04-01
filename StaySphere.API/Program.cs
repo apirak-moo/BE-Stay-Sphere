fd6d85a0074254e9352ce9465eef7aded27042fc
Apirak Soimaree <apirak.soi@outlook.com>
2026-04-01 23:48:40 +0700

    Add project files.

---
+++ StaySphere.API/Program.cs
@@ -0,0 +1,13 @@
+using StaySphere.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Startup startup = new(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+startup.Configure(app, builder.Environment);
+
+app.Run();
---
+++ StaySphere.API/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Scalar.AspNetCore;
+using StaySphere.Application;
+using StaySphere.Infrastructure;
+
+namespace StaySphere.API;
+
+public class Startup
+{
+
+    private IConfiguration configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddInfrastructure(configuration)
+                .AddApplication();
+        services.AddControllers();
+        services.AddOpenApi();
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        app.UseRouting();
+        if (env.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.MapScalarApiReference();
+        }
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+
+}
